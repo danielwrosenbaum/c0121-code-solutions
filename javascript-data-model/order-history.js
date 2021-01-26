0c4aa6f27526de7825129029ebc7301eba361741
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-01-26 11:37:07 -0800

    Completed with more changes

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,61 +1,34 @@
 var orderHistory = [
   {
-    title: 'Javascript for impatient programmers',
-    type: 'book',
-    author: 'Dr. Axel Rauschmayer',
-    totalCost: 34.00,
-    itemCost: 31.55,
+    orderNumber: '114-3941689-8772232',
     orderPlaced: 'August 4, 2020',
     orderDelivered: 'August 8, 2020',
     returnWindowClosed: 'September 7, 2020',
-    orderNumber: '114-3941689-8772232'
+    totalCost: 34.00,
+    items: 'Javascript for Impatient Programmers'
   },
   {
-    title: 'The Timeless Way of Building',
-    type: 'book',
-    author: 'Christopher Alexander',
-    totalCost: 44.53,
-    itemCost: 41.33,
+    orderNumber: '113-9984268-1280257',
     orderPlaced: 'July 19, 2020',
     orderDelivered: 'July 20, 2020',
     returnWindowClosed: 'August 19, 2020',
-    orderNumber: '113-9984268-1280257'
+    totalCost: 44.53,
+    items: 'The Timeless Way of Building'
   },
   {
-    title: 'Gamecuber Controller Adapter',
-    type: 'video game accessory',
-    author: '',
-    totalCost: 17.22,
-    itemCost: 15.98,
+    orderNumber: '114-28575557-9059409',
     orderPlaced: 'July 4, 2020',
     orderDelivered: 'July 7, 2020',
     returnWindowClosed: 'August 5, 2020',
-    orderNumber: '114-28575557-9059409'
+    totalCost: 17.22,
+    items: 'Gamecube Controller Adapter'
   },
   {
-    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-    type: 'video game accessory',
-    author: '',
-    totalCost: 138.93,
-    itemCost: 94.95,
+    orderNumber: '113-2883177-2648248',
     orderPlaced: 'July 3, 2020',
     orderDelivered: 'July 5, 2020',
     returnWindowClosed: 'August 4, 2020',
-    orderNumber: '113-2883177-2648248'
-  },
-  {
-    title: 'The Art of Sql',
-    type: 'book',
-    author: 'Stephanie Faroult',
     totalCost: 138.93,
-    itemCost: 33.99,
-    orderPlaced: 'July 3, 2020',
-    orderDelivered: 'July 5, 2020',
-    returnWindowClosed: 'August 4, 2020',
-    orderNumber: '113-2883177-2648248'
+    items: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql']
   }
 ];
-console.log(orderHistory[4].title);
-console.log(orderHistory[3].type);
-console.log(orderHistory[3].itemCost);
-console.log(orderHistory[4].returnWindowClosed);
