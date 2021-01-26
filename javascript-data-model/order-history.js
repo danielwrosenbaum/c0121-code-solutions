a55ef7685e07813640e81c830f368c1304bfcc86
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-01-26 11:50:19 -0800

    Completed with better changes

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -5,7 +5,14 @@ var orderHistory = [
     orderDelivered: 'August 8, 2020',
     returnWindowClosed: 'September 7, 2020',
     totalCost: 34.00,
-    items: 'Javascript for Impatient Programmers'
+    items: [
+      {
+        type: 'book',
+        title: 'Javascript for Impatient Programmers',
+        author: 'Dr. Axel Rauschmayer',
+        itemCost: 31.55
+      }
+    ]
   },
   {
     orderNumber: '113-9984268-1280257',
@@ -13,7 +20,14 @@ var orderHistory = [
     orderDelivered: 'July 20, 2020',
     returnWindowClosed: 'August 19, 2020',
     totalCost: 44.53,
-    items: 'The Timeless Way of Building'
+    items: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        itemCost: 41.33
+      }
+    ]
   },
   {
     orderNumber: '114-28575557-9059409',
@@ -21,7 +35,14 @@ var orderHistory = [
     orderDelivered: 'July 7, 2020',
     returnWindowClosed: 'August 5, 2020',
     totalCost: 17.22,
-    items: 'Gamecube Controller Adapter'
+    items: [
+      {
+        type: 'video game accesory',
+        title: 'Gamecube Controller Adapter',
+        author: undefined,
+        itemCost: 15.98
+      }
+    ]
   },
   {
     orderNumber: '113-2883177-2648248',
@@ -29,6 +50,22 @@ var orderHistory = [
     orderDelivered: 'July 5, 2020',
     returnWindowClosed: 'August 4, 2020',
     totalCost: 138.93,
-    items: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql']
+    items: [
+      {
+        type: 'video game accessory',
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: undefined,
+        itemCost: 94.95
+      },
+      {
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Stephane Faroult',
+        itemCost: 33.99
+      }
+    ]
   }
 ];
+console.log(orderHistory[3].items[1].title);
+console.log(orderHistory[3].items[0].author);
+console.log(orderHistory[3].items[1].itemCost);
