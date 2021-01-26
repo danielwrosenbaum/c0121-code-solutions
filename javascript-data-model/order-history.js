edf02bacbe0d7dd862fe8d5688bb2789b33645cb
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-01-26 11:02:42 -0800

    Completed Javascript Data Model Exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,50 @@
+var orderHistory = [
+  {
+    title: 'Javascript for impatient programmers',
+    type: 'book',
+    author: 'Dr. Axel Rauschmayer',
+    totalCost: 34.00,
+    itemCost: 31.55,
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'August 8, 2020',
+    returnWindowClosed: 'September 7, 2020',
+    orderNumber: '114-3941689-8772232'
+  },
+  {
+    title: 'The Timeless Way of Building',
+    type: 'book',
+    author: 'Christopher Alexander',
+    totalCost: 44.53,
+    itemCost: 41.33,
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'July 20, 2020',
+    returnWindowClosed: 'August 19, 2020',
+    orderNumber: '113-9984268-1280257'
+  },
+  {
+    title: 'Gamecuber Controller Adapter',
+    type: 'video game accessory',
+    author: '',
+    totalCost: 17.22,
+    itemCost: 15.98,
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'July 7, 2020',
+    returnWindowClosed: 'August 5, 2020',
+    orderNumber: '114-28575557-9059409'
+  },
+  {
+    title: ['The Art of Sql', 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)'],
+    type: ['book', 'video game accessory'],
+    author: [' Stephanie Faroult', ''],
+    totalCost: 138.93,
+    itemCost: [33.99, 94.95],
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'July 5, 2020',
+    returnWindowClosed: 'August 4, 2020',
+    orderNumber: '113-2883177-2648248'
+  }
+];
+console.log(orderHistory[3].title[1]);
+console.log(orderHistory[3].type[0]);
+console.log(orderHistory[3].itemCost[1]);
+console.log(orderHistory[3].returnWindowClosed);
