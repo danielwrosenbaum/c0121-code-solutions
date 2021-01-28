101d3093b35b0a9b927f4ef386c017a85361c0b5
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-01-27 16:01:12 -0800

    Completed Javascript-if

---
+++ javascript-if/main.js
@@ -0,0 +1,67 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function startsWithJ(string) {
+  if (string[0] === 'J') {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+function categorizeAcidity(ph) {
+  if (ph === 7) {
+    return 'ph is neutral';
+  } else if (ph < 7) {
+    return 'ph is acidic';
+  } else if (ph > 7 && ph < 14) {
+    return 'ph is base';
+  } else {
+    return 'invalid ph level';
+  }
+}
+function introduceWarnerBro(name) {
+  if (name === 'yakko' || name === 'wakko') {
+    return "We're the warner brothers!";
+  } else if (name === 'dot') {
+    return "I'm Cute~";
+  } else {
+    return 'Goodnight everybody!';
+  }
+}
