aaa69ae7d02695ea98d3bab4a5eec34b9412010c
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-03 22:26:22 -0800

    More work on typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,25 +1,14 @@
-var keys = Array.from(document.querySelectorAll('span'));
-var keyText = document.querySelector('.text');
+var allLetters = document.querySelectorAll('span');
+// var keyText = document.querySelector('span.text');
 
-document.addEventListener('DOMContentLoaded', init);
-function init() {
-  var text = document.querySelector('.text');
-  text.addEventListener('keydown', anyKey);
-  document.body.addEventListener('keydown', anyKey);
-}
-
-function anyKey(event) {
-  // console.log( ev.type, ev.target);
-  // var char =  event.charCode || event.which;
-  // var s = String.fromCharCode(char);
-  for (var i = 0; i < keys.length; i++) {
-    if (keys[i] === event.key) {
-      keys.className = 'text green underline';
-
+function changeColor(event) {
+  for (var i = 0; i < allLetters.length; i++) {
+    allLetters[i].className = 'text underline';
+    if (event.key === allLetters[i].textContent) {
+      allLetters[i].className = 'text green';
+    } else if (event.key !== allLetters[i].textContent) {
+      allLetters.className = 'text red';
     }
   }
 }
-function changeColor(event) {
-  keyText.className = 'text green underline';
-}
-keyText.addEventListener('keydown', changeColor);
+document.addEventListener('keydown', changeColor);
