34ca423d05eb5cc822a7e98c595fa6b00175f539
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-06 20:31:47 -0800

    Tweaked it a little bit

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,38 @@
 var allLetters = document.querySelectorAll('span');
-// var question = document.querySelector('h2');
+var top = document.querySelector('.top');
 var i = 0;
+
 document.addEventListener('keydown', function (event) {
   if (event.key === allLetters[i].textContent) {
     allLetters[i].className = 'green';
     i++;
-    allLetters[i].className = 'black-underline';
-  } else {
+    if (i < allLetters.length - 1) {
+      allLetters[i].className = 'black-underline';
+    } else {
+      allLetters[i].className = 'text';
+      top.className = 'top text';
+      // return i;
+    }
+  } else if (event.key !== allLetters[i].textContent) {
     allLetters[i].className = 'red-underline';
 
   }
+  return i;
 }
 );
+var yesButton = document.querySelector('.yes');
+function reload(event) {
+  window.location.reload();
+}
+yesButton.addEventListener('click', reload);
+
+var noButton = document.querySelector('.no');
+var kBye = document.querySelector('.bye');
+var finish = document.querySelector('.finish-line');
+var wordz = document.querySelector('.wordz');
+function bye(event) {
+  kBye.className = 'bye';
+  finish.className = 'hidden';
+  wordz.className = 'hidden';
+}
+noButton.addEventListener('click', bye);
