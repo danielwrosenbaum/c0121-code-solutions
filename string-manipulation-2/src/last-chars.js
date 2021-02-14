f5bc7fead398a6e264871d7ebb24906989a8146a
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-14 12:48:05 -0800

    Completed lastChars Function

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,12 +1,6 @@
 /* exported lastChars */
 function lastChars(length, string) {
-  var newString = '';
-  for (var i = 0; i < string.length; i++) {
-    if (i < string.length - length) {
-      newString += i;
-      return newString;
-    }
-  }
-  // console.log(newString);
-  return newString;
+  var lastStrings = string.slice(-length);
+
+  return lastStrings;
 }
