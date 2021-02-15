6c143047df3dd0286ae04f3ff96a26cdb42a79ab
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-14 19:15:17 -0800

    Finished pick exercise of Object Manipulation 2

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,15 @@
 /* exported pick */
+function pick(source, keys) {
+  var newObj = {};
+  for (var property in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (property === keys[i]) {
+        newObj[property] = source[property];
+      }
+      if (newObj[property] === undefined) {
+        delete newObj[property];
+      }
+    }
+  }
+  return newObj;
+}
