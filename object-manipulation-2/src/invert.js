3cefb53301091ffab280eae77f97bc43fbb04960
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-14 18:25:13 -0800

    Finished invert exercise of Object Manipulation 2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,9 @@
 /* exported invert */
+
+function invert(source) {
+  var newObj = {};
+  for (var property in source) {
+    newObj[source[property]] = property;
+  }
+  return newObj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,19 +1,1 @@
 /* exported pick */
-function pick(source, keys) {
-  var newObj = {};
-  // var objValues = Object.values(source);
-  // console.log('values ', objValues);
-  var i = 0;
-  for (i; i < keys.length; i++) {
-    // console.log('keys', keys, 'i:', keys[i]);
-    for (var property in source) {
-      if (property === keys[i]) {
-        newObj = Object.assign({}, property);
-        // console.log('newObj: ', newObj);
-
-      }
-
-    }
-  }
-  return newObj;
-}
