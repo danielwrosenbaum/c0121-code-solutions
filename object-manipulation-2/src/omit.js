38b7f25fd6b9bc21683006a80c309d8463f217f5
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-15 09:36:43 -0800

    Completed Omit exercise of Object Manipulation 2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,23 +1,13 @@
 /* exported omit */
-// function omit(source, keys) {
-//   var newObj = {};
-//   for (var property in source) {
-//     console.log('prop: ', property);
-//     for (var i = 0; i < keys.length; i++) {
-//       console.log('keys ', keys[i]);
-//       newObj[property] = source[property];
-//       console.log('new p', newObj);
-
-//     }
-
-//     // if (newObj[property] === undefined) {
-//     //   delete newObj[property];
-//     // }
-//   }
-//   if (newObj[property] === keys[i]) {
-//     delete newObj.property;
-//   }
-//   console.log('new', newObj);
-//   return newObj;
-
-// }
+function omit(source, keys) {
+  var newObj = {};
+  for (var property in source) {
+    newObj[property] = source[property];
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] === property) {
+        delete newObj[property];
+      }
+    }
+  }
+  return newObj;
+}
