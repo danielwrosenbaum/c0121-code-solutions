b4198fbef96103562a305c3fcae966f26d6a1854
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-20 01:28:55 +0000

    Completed isPalindromic exercise

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,1 +1,14 @@
 /* exported isPalindromic */
+function isPalindromic(string) {
+  var noSpaces = string.replaceAll(" ", '')
+  var i = 0;
+  var j = noSpaces.length -1;
+  for (i; i <noSpaces.length; i++) {
+  if (noSpaces[i] === noSpaces[j]) {
+    j--;
+  } else {
+    return false;
+  }
+}
+  return true;
+}
