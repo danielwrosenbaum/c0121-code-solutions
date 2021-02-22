c393157ea556f08c780e535629440ac524fd7f4e
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-22 02:51:32 +0000

    Finished isAnagram Exercise

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,16 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  var firstNoSpaces = firstString.replaceAll(" ", '');
+  var secondNoSpace = secondString.replaceAll(" ", '');
+  var firstArray = firstNoSpaces.split('').sort();
+  var secondArray = secondNoSpace.split('').sort();
+  console.log('first: ', firstArray, 'second: ', secondArray);
+  for (var i =0; i < firstArray.length; i++) {
+    if (firstArray[i] === secondArray[i]) {
+      i++;
+  } else {
+    return false;
+  }
+  }
+  return true;
+  }
