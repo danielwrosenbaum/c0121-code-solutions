99d7b69ed760e5dc7e6a62b4fe228786935f6333
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-22 05:52:27 +0000

    Worked on flatten exercise

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,14 @@
 /* exported flatten */
+function flatten(array) {
+  var newArray = [];
+  for (var i =0; i < array.length; i++) {
+    if (array[i] >= 1) {
+      newArray.push(array[i]);
+    }
+    else {
+      for (var j =0; j < array[i].length; j++)
+    newArray.push(array[i][j]);
+    }
+  }
+  return newArray;
+}
