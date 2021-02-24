252fefd86fb90614ca94774cac0234e132f0dc94
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-02-24 17:43:33 +0000

    Started work on zip exercise

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,17 @@
 /* exported zip */
+function zip(first, second) {
+  var x = 0;
+  if (first.length <= second.length) {
+    var newArray = new Array(first.length)
+    for (var i =0; i < first.length; i++) {
+     newArray[x].push(first[i], second[i])
+    //  x++;
+    //  for (var j =0; j < second.length; j++) {
+    //    newArray.push(second[j])
+      //  x++;
+    //  }
+
+    }
+console.log(newArray)
+  }
+}
