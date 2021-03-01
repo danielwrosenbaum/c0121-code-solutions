bfe134ea898e070f0bed4409ef57d0aafeaf02ce
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-01 04:45:04 +0000

    Finished union exercise.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,29 @@
 /* exported difference */
+// function difference(first, second) {
+//   var newArray = first.concat(second);
+//   console.log(newArray)
+//   var returnArray = []
+//   for (var i =0; i < newArray; i++) {
+//     var newSet = new Set(newArray[i]);
+//     returnArray = Array.from(newSet);
+//   }
+//   return returnArray;
+// }
+
+function difference(first, second) {
+ var newArray = first.concat(second);
+ var newSet = new Set(newArray);
+var returnArray = Array.from(newSet);
+return returnArray
+}
+
+// function difference(first, second) {
+//   var newArray = [];
+//   first.forEach((e1) =>second.forEach((e2) => {
+//     if (e1 === e2) {
+//       newArray.push(e1)
+//     }
+//   }
+//   ))
+//   return newArray
+// }
