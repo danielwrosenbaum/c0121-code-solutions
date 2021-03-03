04c34096340933bdd59526b6a401a2018cde9bc2
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-03 04:51:40 +0000

    Fixed zip exercise.

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,18 +1,10 @@
 /* exported zip */
 function zip(first, second) {
   var newArray =[];
-  if (first.length <= second.length) {
-    newArray.length = first.length;
-    for (var i = 0; i < first.length; i++) {
+  var length = Math.min(first.length, second.length);
+    for (var i = 0; i < length; i++) {
       newArray[i] = [];
-      newArray[i].push(first[i], second[i])
-    }
-  } else {
-    newArray.length = second.length
-    for (var j = 0; j < second.length; j++) {
-      newArray[j] = [];
-      newArray[j].push(first[j], second[j]);
-    }
+      newArray[i].push(first[i], second[i]);
   }
   return newArray;
 }
