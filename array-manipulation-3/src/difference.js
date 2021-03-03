3b60937e3f179941fb1f8dab989e4b41d9d98f84
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-03 04:49:35 +0000

    Fixed difference Exercise.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,15 +1,20 @@
 /* exported difference */
 function difference(first, second) {
 var newArray = [];
+var newArray2 = []
 var returnArray = [];
 for (var i = 0; i < first.length; i++) {
   var j = second.indexOf(first[i]);
-  if (j === -1) {
+  if (j === -1 ) {
     newArray.push(first[i]);
-  } else {
-    second.splice(j, 1);
   }
 }
-returnArray = newArray.concat(second);
+for (var x=0; x< second.length; x++) {
+  var y = first.indexOf(second[x]);
+  if (y === -1) {
+    newArray2.push(second[x])
+  }
+}
+returnArray = newArray.concat(newArray2);
 return returnArray;
 }
