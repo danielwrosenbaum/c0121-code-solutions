19fdc1dbcf61d2fc328cb952065125075e990360
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-06 01:30:05 +0000

    started work on collections

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -1,1 +1,16 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var playerOne = {
+  hand: []
+};
+var playerTwo = {
+  hand: []
+}
+var playerThree = {
+  hand: []
+}
+var playerFour = {
+  hand: [];
+}
+var suits = ["spades", "diamonds", "clubs", "hearts"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
