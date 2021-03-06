edec713ce3e63c803c01a87ab03dc616d420828f
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-06 02:17:15 +0000

    more work on collections

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -5,12 +5,35 @@ var playerOne = {
 };
 var playerTwo = {
   hand: []
-}
+};
 var playerThree = {
   hand: []
-}
+};
 var playerFour = {
-  hand: [];
+  hand: []
+};
+var suit = ['spades', 'diamonds', 'clubs', 'hearts'];
+var rank = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+
+function createDeck() {
+  var deck = [];
+  for (var i = 0; i < suit.length; i++) {
+    for (var j = 0; j < rank.length; j++) {
+      var card = {
+        cardRank: rank[j],
+        cardSuit: suit[i]
+      };
+      deck.push(card);
+    }
+  }
+  console.log(deck, card);
+  return deck;
 }
-var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function shuffleCards(deck) {
+  for (var i = deck.length - 1; i > 0; i--) {
+    var randomNum = Math.floor(Math.random() * (i + 1));
+
+  }
+
+}
