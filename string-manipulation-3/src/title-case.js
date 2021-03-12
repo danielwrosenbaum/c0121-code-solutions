1539bfe2a195005c3389c8e6c1c54ffcf766239a
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-12 20:31:51 +0000

    Finally Completed String Manipulation 3 - Daniel Rosenbaum.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,19 +1,32 @@
 /* exported titleCase */
 function titleCase(title) {
-  var stringArray = title.split(" ");
-  console.log(stringArray)
+  var stringArray = title.toLowerCase();
+  stringArray = stringArray.split(' ');
   for (var i = 0; i < stringArray.length; i++) {
-    // stringArray[0][0] = stringArray[0][0].toUpperCase();
-    if (stringArray[i] === 'javascript') {
-      stringArray[i] = 'JavaScript';
-    }
-    else if (stringArray[i] === 'api') {
-      stringArray[i] = 'API';
-    }
-    else if (stringArray[i] !== 'in' && stringArray[i] !=='JavaScript' && stringArray[i] !== 'for' && stringArray[i] !== 'and' && stringArray[i] !== 'of' && stringArray[i] !== 'the') {
-    stringArray[i] = stringArray[i][0].toUpperCase() + stringArray[i].substr(1);
+    for (var j = 0; j < stringArray[i].length; j++) {
+      if (stringArray[i][j] === '-') {
+        stringArray[i] = stringArray[i].split('-');
+        stringArray[i][1] = stringArray[i][1].charAt(0).toUpperCase() + stringArray[i][1].slice(1);
+        stringArray[i] = stringArray[i].join('-');
+      }
+      var x = stringArray[i].length - 1;
+      if (stringArray[i] === 'javascript') {
+        stringArray[i] = 'JavaScript';
+      } else if (stringArray[i] === 'javascript:') {
+        stringArray[i] = 'JavaScript:';
+      } else if (stringArray[i] === 'api') {
+        stringArray[i] = 'API';
+      } else {
+        if (stringArray[i].charAt(x) !== ':' && stringArray[i] !== 'in' && stringArray[i] !== 'for' && stringArray[i] !==
+    'and' && stringArray[i] !== 'of' && stringArray[i] !== 'the') {
+          stringArray[i] = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
+        }
+      }
+      if (stringArray[i].charAt(x) === ':') {
+        stringArray[i + 1] = stringArray[i + 1].charAt(0).toUpperCase() + stringArray[i + 1].slice(1);
+      }
     }
   }
-
-  return stringArray.join(' ');
+  var joinArray = stringArray.join(' ');
+  return joinArray;
 }
