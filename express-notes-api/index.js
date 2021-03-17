ab8270a07f2b20cacf4fe67d0587e9d0b10c5fa1
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-17 02:42:59 +0000

    Completed all the express parts of exercise. Now to node.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -26,6 +26,54 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
+app.use(express.json());
+app.post('/api/notes', (req, res) => {
+  const newObj = {
+    id: jsonData.nextId,
+    content: req.body.content
+  };
+  if (req.body.content === undefined) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (req.body.content !== undefined) {
+    jsonData.notes[jsonData.nextId] = newObj;
+    jsonData.nextId++;
+    res.status(201).send(newObj);
+  } else {
+    res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const index = req.params.id;
+  if (!Math.sign(index)) {
+    res.status(400).json({ error: index + ' ' + 'is not a positive integer' });
+  } else {
+    if (jsonData.notes[index] !== undefined) {
+      delete jsonData.notes[index];
+      res.sendStatus(204);
+    } else {
+      res.status(404).json({ error: 'cannot find note with id' + ' ' + index });
+    }
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const index = req.params.id;
+  const newObj = {
+    id: index,
+    content: req.body.content
+  };
+  if (!Math.sign(index) || req.body.content === undefined) {
+    res.status(400).json({ error: ':-(' });
+  } else {
+    if (jsonData.notes[index] !== undefined) {
+      jsonData.notes[index] = newObj;
+      res.status(200).send(newObj);
+    } else {
+      res.status(404).json({ error: 'cannot find note with id' + ' ' + index });
+    }
+  }
+});
 app.listen(3000, () => {
   // console.log('listening on port 3000!');
 });
