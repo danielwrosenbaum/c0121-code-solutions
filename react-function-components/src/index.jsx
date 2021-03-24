6ed815f7ce3b3dbdc3eab52982c7b369fd9620e9
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-24 19:55:27 +0000

    Completed the assignment correctly this time.

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -5,5 +5,5 @@ function CustomButton() {
   return <button>Click Me!</button>;
 }
 const $div = document.getElementById('root');
-const element = CustomButton();
-ReactDOM.render(element, $div);
+
+ReactDOM.render(<CustomButton />, $div);
