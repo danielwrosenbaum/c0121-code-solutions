489790784a87c1e65575152c08cda040c847cbfb
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-24 19:29:56 +0000

    Completed React Function Components Exercise - Daniel Rosenbaum.

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton() {
+  return <button>Click Me!</button>;
+}
+const $div = document.getElementById('root');
+
+ReactDOM.render(CustomButton(), $div);
