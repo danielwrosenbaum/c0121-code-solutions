b711851575e2c42cd42936c7185c3622217a79c2
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-26 01:18:26 +0000

    Completed React Stopwatch Component - Daniel Rosenbaum.

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,6 +4,8 @@ export default class StopWatch extends React.Component {
   constructor(props) {
     super(props);
     this.pressWatch = this.pressWatch.bind(this);
+    this.timer = this.timer.bind(this);
+    this.clear = this.clear.bind(this);
     this.state = {
       count: 0,
       isPressed: false
@@ -14,25 +16,40 @@ export default class StopWatch extends React.Component {
     const isPressed = this.state.isPressed;
     if (!isPressed) {
       this.setState({ isPressed: true });
+      const countUp = setInterval(this.timer, 1000);
+      this.setState({ countUp: countUp });
     } else {
       this.setState({ isPressed: false });
+      clearInterval(this.state.countUp);
     }
+  }
+
+  clear() {
+    const isPressed = this.state.isPressed;
+    if (!isPressed) {
+      this.setState({ count: 0 });
+    }
+  }
+
+  timer() {
+    this.setState({ count: this.state.count + 1 });
   }
 
   render() {
     let action;
+    const count = this.state.count;
     const isPressed = this.state.isPressed;
-    if (isPressed) {
+    if (!isPressed) {
       action = 'fas fa-play';
     } else {
       action = 'fas fa-pause';
 
     }
-
     return (
       <div className={'container'}>
-        <div className={'clock'}>
-        </div>
+        <a className={'clock'} onClick={this.clear}>
+          <div className={'counter'}>{count}</div>
+        </a>
         <a className={'button'} onClick={this.pressWatch}>
           <i className={action}></i>
         </a>
