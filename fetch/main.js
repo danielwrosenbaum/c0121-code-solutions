82bbd0f08c918008a8de5eb08d15e9d6fca410a2
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-03-29 19:19:00 +0000

    Completed Fetch Exercise - Daniel Rosenbaum.

---
+++ fetch/main.js
@@ -0,0 +1,9 @@
+// https:// jsonplaceholder.typicode.com/users
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(response => response.json())
+  .then(data => console.log(data));
