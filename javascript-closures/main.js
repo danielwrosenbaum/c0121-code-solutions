2830af1a7df8cde633c5ed522f5caa5914d1c283
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-04-02 17:52:06 +0000

    Completed JavaScript Closures Exercise - Daniel Rosenbaum.

---
+++ javascript-closures/main.js
@@ -0,0 +1,10 @@
+function graduate(credential) {
+  return function (fullName) {
+    return fullName + ', ' + credential;
+  };
+
+}
+const medicalSchool = graduate('M.D.');
+const lawSchool = graduate('Esq.');
+console.log(medicalSchool('Daniel Rosenbaum'));
+console.log(lawSchool('Daniel Rosenbaum'));
